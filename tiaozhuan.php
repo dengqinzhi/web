89ef995cb843a7a3f53d98c1e995530eab2456db
dengqinzhi <58380125+dengqinzhi@users.noreply.github.com>
2022-12-12 08:46:25 +0800

    跳转html代码

---
+++ tiaozhuan.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta http-equiv="Content-Language" content="zh-CN">
+<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<meta http-equiv="refresh" content="0;url=http://103.40.240.2/">
+<title>老邓的博客</title>
+</head>
+<body>
+
+</body>
+</html>  
