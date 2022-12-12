c7a115f71510d17e9ef7f90c59e76b1783a8855b
dengqinzhi <58380125+dengqinzhi@users.noreply.github.com>
2022-12-12 09:00:54 +0800

    网页插入网易云音乐代码

---
+++ wangyiyun music.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+var s = unescape("\u0069\u0066\u0072\u0061\u006d\u0065");
+var f = '<'+s+' frameborder="no" border="0" marginwidth="0" marginheight="0" width="280" height="52" src="//music.163.com/outchain/player?type=0&id=3026838792&auto=1&height=32" class="music" style="z-index: 0;right: 0PX;display: block;bottom: 90px;position: fixed;"></'+s+'>';
+$("body").append(f); //网易云音乐  </script>
