96c5131f4ebd1a5703e483b9c4437bf59fa393d3
dengqinzhi <24427467@qq.com>
2023-07-15 16:25:25 +0800

    Create tianqi-ip.js

---
+++ tianqi-ip.js
@@ -0,0 +1,6 @@
+网页尾添加获取ip天气代码
+  
+<iframe src="https://cha.ebaitian.cn/plus/getinfo" height="60" width="100%" marginheight="0" marginwidth="0" frameborder="0" align="middle" allowtransparency="true" scrolling="no"></iframe>
+
+
+height="60" width="100%" 根据实际大小调节
